b3c2949150ab4d1f426a888dcb1c0e56bd117240
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-15 16:43:45 +0700

    style(header): setup header style

--- Website/13_skincare/src/components/Header/Header.tsx
+++ Website/13_skincare/src/components/Header/Header.tsx
@@ -1,18 +1,27 @@
 import React from "react";
 import css from "./Header.module.css";
 import Logo from "../../assets/logo.png";
+import { CgShoppingBag } from "react-icons/cg";
 
-const Header = () => {
+const Header: React.FC = () => {
   return (
     <div className={css.container}>
       <div className={css.logo}>
-        <img src={Logo} alt="logo" />
+        <img src={Logo} alt="Amazon Logo" />
         <span>amazon</span>
       </div>
       <div className={css.right}>
-        <div className={css.menu}>menu</div>
-        <input type="text" className={css.search} />
-        <span>PNG icon of cart</span>
+        <div className={css.menu}>
+          <ul className={css.menu}>
+            <li>Collections</li>
+            <li>Brands</li>
+            <li>New</li>
+            <li>Sales</li>
+            <li>ENG</li>
+          </ul>
+        </div>
+        <input type="text" className={css.search} placeholder="Search" />
+        <CgShoppingBag className={css.cart} />
       </div>
     </div>
   );
