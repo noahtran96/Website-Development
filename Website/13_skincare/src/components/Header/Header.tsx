c8f797dc4f4876a9949b9d1179b794c4ef2dca21
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-15 16:17:08 +0700

    style(app): setup body styles and fonts

--- Website/13_skincare/src/components/Header/Header.tsx
+++ Website/13_skincare/src/components/Header/Header.tsx
@@ -1,8 +1,17 @@
 import React from "react";
-import "./Header.module.css";
+import css from "./Header.module.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className={css.container}>
+      <div className={css.logo}>logo</div>
+      <div className={css.right}>
+        <div className={css.menu}></div>
+        <input type="text" className={css.search} />
+        <span>PNG icon of cart</span>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
