f0eece634235ec79daa18265fa59b5fa30e7303d
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-15 16:19:44 +0700

    feat(header): setup html structure

--- Website/13_skincare/src/components/Header/Header.tsx
+++ Website/13_skincare/src/components/Header/Header.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import css from "./Header.module.css";
+import Logo from "../../assets/logo.png";
 
 const Header = () => {
   return (
     <div className={css.container}>
-      <div className={css.logo}>logo</div>
+      <div className={css.logo}>
+        <img src={Logo} alt="logo" />
+        <span>amazon</span>
+      </div>
       <div className={css.right}>
-        <div className={css.menu}></div>
+        <div className={css.menu}>menu</div>
         <input type="text" className={css.search} />
         <span>PNG icon of cart</span>
       </div>
