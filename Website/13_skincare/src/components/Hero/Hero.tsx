4c9f6ff4639bcf7db5198249a0718355e21879b9
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-16 22:24:05 +0700

    feat(hero): setup html structure

--- Website/13_skincare/src/components/Hero/Hero.tsx
+++ Website/13_skincare/src/components/Hero/Hero.tsx
@@ -1,8 +1,52 @@
 import React from "react";
 import css from "./Hero.module.css";
+import HeroImg from "../../assets/hero.png";
+import { RiShoppingBagFill } from "react-icons/ri";
+import { BsArrowRight } from "react-icons/bs";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <div className={css.container}>
+      {/* left side */}
+      <div className={css.h_sides}>
+        <span>skin protection cream</span>
+        <div className={css.text2}>
+          <span>Trendy Collection</span>
+          <span>
+            Pure ingredients for your natural glow. Experience the ultimate skin
+            transformation today.
+          </span>
+        </div>
+      </div>
+
+      {/* middle side */}
+      <div className={css.wrapper}>
+        <div className={css.blueCircle}></div>
+        <img src={HeroImg} alt="Hero Image" width={600} />
+        <div className={css.cart2}>
+          <RiShoppingBagFill />
+          <div className={css.signup}>
+            <span>Best Signup Offers</span>
+            <div>
+              <BsArrowRight />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* right side */}
+      <div className={css.h_sides}>
+        <div className={css.traffic}>
+          <span>1.5m</span>
+          <span>Monthly Traffic</span>
+        </div>
+        <div className={css.customers}>
+          <span>100k</span>
+          <span>Happy Customers</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
