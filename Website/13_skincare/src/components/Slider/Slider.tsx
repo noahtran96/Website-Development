816a5771bd47c849e881e44a4f43cfd1fb3d405f
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-17 18:00:15 +0700

    feat(slider): setup html structure

--- Website/13_skincare/src/components/Slider/Slider.tsx
+++ Website/13_skincare/src/components/Slider/Slider.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
-import "./Slider.css"
+import React from 'react';
+import {Swiper, SwiperSlide} from "swiper/react";
+import {Pagination, Navigation} from "swiper/modules";
+import "swiper/css/bundle";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "./Slider.css";
+import {SliderProducts} from "../../constants/products";
 
 const Slider = () => {
   return (
-    <div>Slider</div>
+    <div className="s-container">
+        <Swiper slidesPerView={3} spaceBetween={40} slidesPerGroup={1} loop={true}>
+            {SliderProducts.map((slide, index) => (
+                <SwiperSlide key={index}>
+                    <div className="left-s">
+                        <div className="name">
+                            <span>{slide.name}</span>
+                            <span>{slide.detail}</span>
+                        </div>
+                        <span>{slide.price}</span>
+                        <div>Shop now</div>
+                    </div>
+                    <img src={slide.img} alt="Product Image" className="img-p"/>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    </div>
   )
 }
 
