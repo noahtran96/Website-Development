061507b30e685dbe752c632323462d136e63ccfe
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-17 17:01:51 +0700

    chore(slider): setup folder structure and import libraries

--- Website/13_skincare/src/App.tsx
+++ Website/13_skincare/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Slider from "./components/Slider/Slider";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <Slider/>
     </div>
   );
 };
