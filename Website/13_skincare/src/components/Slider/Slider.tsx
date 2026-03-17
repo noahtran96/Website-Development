b3d3313a10d6158657a76811f18a1aa5e2a30ee1
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-17 18:33:13 +0700

    style(slider): add css styling

--- Website/13_skincare/src/components/Slider/Slider.tsx
+++ Website/13_skincare/src/components/Slider/Slider.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Swiper, SwiperSlide} from "swiper/react";
 import {Pagination, Navigation} from "swiper/modules";
-import "swiper/css/bundle";
+import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./Slider.css";
@@ -10,15 +10,15 @@ import {SliderProducts} from "../../constants/products";
 const Slider = () => {
   return (
     <div className="s-container">
-        <Swiper slidesPerView={3} spaceBetween={40} slidesPerGroup={1} loop={true}>
-            {SliderProducts.map((slide, index) => (
+        <Swiper modules={[Pagination, Navigation]} className="my-swiper" slidesPerView={3} navigation={true} spaceBetween={40} slidesPerGroup={1} loop={true}>
+            {[...SliderProducts, ...SliderProducts].map((slide, index) => (
                 <SwiperSlide key={index}>
                     <div className="left-s">
                         <div className="name">
                             <span>{slide.name}</span>
                             <span>{slide.detail}</span>
                         </div>
-                        <span>{slide.price}</span>
+                        <span>${slide.price}</span>
                         <div>Shop now</div>
                     </div>
                     <img src={slide.img} alt="Product Image" className="img-p"/>
