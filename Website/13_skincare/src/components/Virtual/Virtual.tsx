e05f60592a4a6747216f908a762e3e4418b117b4
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-18 19:50:40 +0700

    feat(virtual): setup html structure for left section

--- Website/13_skincare/src/components/Virtual/Virtual.tsx
+++ Website/13_skincare/src/components/Virtual/Virtual.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import css from "./Virtual.module.css";
+import Shade from "../../assets/shade.png";
 
 const Virtual = () => {
-  return <>Virtual</>;
+  return (
+    <div className={css.Virtual}>
+      <div className={css.left}>
+        <span>virtual try-one</span>
+        <span>Never Buy the wrong Shade Again!</span>
+        <span>Try Now!</span>
+        <img src={Shade} alt="Blue Shade" />
+      </div>
+      <div className={css.right}>Virtual component</div>
+    </div>
+  );
 };
 
 export default Virtual;
