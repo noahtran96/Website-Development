62b7677ac53fb8cf6f84f06539ce56fdff9b12b7
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-18 20:08:43 +0700

    style(virtual): add css styling to right section

--- Website/13_skincare/src/components/Virtual/Virtual.tsx
+++ Website/13_skincare/src/components/Virtual/Virtual.tsx
@@ -1,17 +1,26 @@
 import React from "react";
 import css from "./Virtual.module.css";
 import Shade from "../../assets/shade.png";
+import ReactCompareImage from "react-compare-image";
+import Before from "../../assets/before.png";
+import After from "../../assets/after.png";
 
 const Virtual = () => {
   return (
     <div className={css.Virtual}>
+      {/* left side */}
       <div className={css.left}>
         <span>virtual try-on</span>
         <span>never buy the wrong shade again!</span>
         <span>Try Now!</span>
         <img src={Shade} alt="Blue Shade" />
       </div>
-      <div className={css.right}>Virtual component</div>
+      {/* right side */}
+      <div className={css.right}>
+        <div className={css.wrapper}>
+          <ReactCompareImage leftImage={Before} rightImage={After} />
+        </div>
+      </div>
     </div>
   );
 };
