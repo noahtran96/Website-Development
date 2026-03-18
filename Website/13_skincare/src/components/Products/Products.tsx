d30563c2e92484cde4ccaa279463f242acd74e07
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-18 20:21:29 +0700

    feat(products): setup html structure for title section

--- Website/13_skincare/src/components/Products/Products.tsx
+++ Website/13_skincare/src/components/Products/Products.tsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./Products.module.css";
+import Plane from "../../assets/plane.png";
+import { ProductsData } from "../../constants/products";
 
 const Products = () => {
-  return <div>Products</div>;
+  const [MenuProducts, setMenuProducts] = useState(ProductsData);
+  return (
+    <div className={css.container}>
+      <img src={Plane} alt="Plane" />
+      <h1>Our Featured Products</h1>
+
+      <div className={css.products}>
+        <ul className={css.menu}>
+          <li>All</li>
+          <li>Skincare</li>
+          <li>Conditioners</li>
+          <li>Foundations</li>
+        </ul>
+        <div className={css.list}>
+          {MenuProducts.map((product, index) => (
+            <div key={index} className={css.product}>
+              Products
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
