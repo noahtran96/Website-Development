f712ce003e6c10f73284ccd818fc8f88238f9bbc
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-19 08:49:52 +0700

    feat(header): make header desktop responsive

--- Website/13_skincare/src/components/Header/Header.tsx
+++ Website/13_skincare/src/components/Header/Header.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./Header.module.css";
 import Logo from "../../assets/logo.png";
 import { CgShoppingBag } from "react-icons/cg";
+import { GoRows } from "react-icons/go";
 
 const Header: React.FC = () => {
+  const [ShowMenu, setShowMenu] = useState(true);
+  const toggleMenu = () => {
+    setShowMenu((ShowMenu) => !ShowMenu);
+  };
   return (
     <div className={css.container}>
       <div className={css.logo}>
@@ -11,15 +16,19 @@ const Header: React.FC = () => {
         <span>amazon</span>
       </div>
       <div className={css.right}>
-        <div className={css.menu}>
-          <ul className={css.menu}>
-            <li>Collections</li>
-            <li>Brands</li>
-            <li>New</li>
-            <li>Sales</li>
-            <li>ENG</li>
-          </ul>
+        <div className={css.bars} onClick={toggleMenu}>
+          <GoRows />
         </div>
+        <ul
+          className={css.menu}
+          style={{ display: ShowMenu ? "flex" : "none" }}
+        >
+          <li>Collections</li>
+          <li>Brands</li>
+          <li>New</li>
+          <li>Sales</li>
+          <li>ENG</li>
+        </ul>
         <input type="text" className={css.search} placeholder="Search" />
         <CgShoppingBag className={css.cart} />
       </div>
