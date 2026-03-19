21f67237808369a916df81527a78ba1a56d8e417
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-19 09:27:36 +0700

    feat(slider): make mobile responsive

--- Website/13_skincare/src/components/Slider/Slider.tsx
+++ Website/13_skincare/src/components/Slider/Slider.tsx
@@ -1,32 +1,41 @@
-import React from 'react';
-import {Swiper, SwiperSlide} from "swiper/react";
-import {Pagination, Navigation} from "swiper/modules";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import "./Slider.css";
-import {SliderProducts} from "../../constants/products";
+import { SliderProducts } from "../../constants/products";
 
 const Slider = () => {
   return (
     <div className="s-container">
-        <Swiper modules={[Pagination, Navigation]} className="my-swiper" slidesPerView={3} navigation={true} spaceBetween={40} slidesPerGroup={1} loop={true}>
-            {[...SliderProducts, ...SliderProducts].map((slide, index) => (
-                <SwiperSlide key={index}>
-                    <div className="left-s">
-                        <div className="name">
-                            <span>{slide.name}</span>
-                            <span>{slide.detail}</span>
-                        </div>
-                        <span>${slide.price}</span>
-                        <div>Shop now</div>
-                    </div>
-                    <img src={slide.img} alt="Product Image" className="img-p"/>
-                </SwiperSlide>
-            ))}
-        </Swiper>
+      <Swiper
+        breakpoints={{ 640: { slidesPerView: 3 }, 0: { slidesPerView: 1 } }}
+        modules={[Pagination, Navigation]}
+        className="my-swiper"
+        slidesPerView={3}
+        navigation={true}
+        spaceBetween={40}
+        slidesPerGroup={1}
+        loop={true}
+      >
+        {[...SliderProducts, ...SliderProducts].map((slide, index) => (
+          <SwiperSlide key={index}>
+            <div className="left-s">
+              <div className="name">
+                <span>{slide.name}</span>
+                <span>{slide.detail}</span>
+              </div>
+              <span>${slide.price}</span>
+              <div>Shop now</div>
+            </div>
+            <img src={slide.img} alt="Product Image" className="img-p" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </div>
-  )
-}
+  );
+};
 
-export default Slider
+export default Slider;
