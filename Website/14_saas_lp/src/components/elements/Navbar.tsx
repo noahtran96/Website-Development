2a9c01098bd0071562804e08e417445a33dd7b4b
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-21 23:29:06 +0700

    feat(container): setup container structure

--- Website/14_saas_lp/src/components/elements/Navbar.tsx
+++ Website/14_saas_lp/src/components/elements/Navbar.tsx
@@ -1,5 +1,26 @@
 import type { FC } from "react";
+import { Container } from "../shared/Container";
+
+const navItems = [
+  { href: "#", text: "Home" },
+  { href: "#services", text: "Services" },
+  { href: "#about-us", text: "About Us" },
+  { href: "#features", text: "Features" },
+];
 
 export const Navbar: FC = () => {
-  return <></>;
+  return (
+    <header className="absolute inset-x-0 top-0 z-50 py-6">
+      <Container>
+        <nav className="w-full flex justify-between gap-6 relative">
+          {/* Logo */}
+          <div className="min-w-max inline-flex relative">
+            <a href="">
+              <img src="" alt="Logo" />
+            </a>
+          </div>
+        </nav>
+      </Container>
+    </header>
+  );
 };
---
+++ Website/14_saas_lp/src/components/shared/Container.tsx
@@ -0,0 +1,16 @@
+import type { FC } from "react";
+
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Container: FC<ContainerProps> = ({ children, className = "" }) => {
+  return (
+    <div
+      className={`mx-auto max-w-7xl w-full px-5 sm:px-8 md:px-14 lg:px-5 ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
