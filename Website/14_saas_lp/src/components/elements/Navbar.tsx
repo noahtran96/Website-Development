c6d51bb08110f138dabe993420e30757788f0b7a
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-23 00:14:12 +0700

    feat(navbar): setup html structure and add css styling

--- Website/14_saas_lp/src/components/elements/Navbar.tsx
+++ Website/14_saas_lp/src/components/elements/Navbar.tsx
@@ -37,6 +37,26 @@ export const Navbar: FC = () => {
               <BtnLink text="Get Started" href="#cta" className="" />
             </div>
           </div>
+
+          {/* Light/Dark Mode Toggle */}
+          <div className="min-w-max flex items-center gap-x-3">
+            <button className="outline-hidden flex relative text-heading-2 rounded-full p-2 lg:p-3 border border-box-border">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                />
+              </svg>
+            </button>
+          </div>
         </nav>
       </Container>
     </header>
