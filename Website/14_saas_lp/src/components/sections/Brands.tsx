518ca1eaeea463302a275c9aaee74860a0370303
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-26 00:31:28 +0700

    feat(constants/brandlogos): setup brand logo constants

--- Website/14_saas_lp/src/components/sections/Brands.tsx
+++ Website/14_saas_lp/src/components/sections/Brands.tsx
@@ -1,8 +1,8 @@
 import type { FC } from "react";
 import { Container } from "../shared/Container";
 import { Title } from "../shared/Title";
-
-const logos = [""];
+import { BRAND_LOGOS } from "../../constants";
+import type { BrandLogo } from "../../constants";
 
 export const Brands: FC = () => {
   return (
@@ -11,7 +11,13 @@ export const Brands: FC = () => {
         <div className="text-center max-w-3xl mx-auto">
           <Title>Trusted by Industry Leaders</Title>
         </div>
-        <div></div>
+        <div>
+          {BRAND_LOGOS.map((logo: BrandLogo) => (
+            <div key={logo.id}>
+              <img src={`assets/logos/${logo.name}.png`} alt={logo.alt} />
+            </div>
+          ))}
+        </div>
       </Container>
     </section>
   );
---
+++ Website/14_saas_lp/src/constants/brandLogos.ts
@@ -0,0 +1,43 @@
+export interface BrandLogo {
+  id: number;
+  name: string;
+  alt: string;
+}
+
+export const BRAND_LOGOS: BrandLogo[] = [
+  {
+    id: 1,
+    name: "discord",
+    alt: "Discord Brand Logo",
+  },
+  {
+    id: 2,
+    name: "openai",
+    alt: "OpenAI Brand Logo",
+  },
+  {
+    id: 3,
+    name: "paypal",
+    alt: "PayPal Brand Logo",
+  },
+  {
+    id: 4,
+    name: "slack",
+    alt: "Slack Brand Logo",
+  },
+  {
+    id: 5,
+    name: "spotify",
+    alt: "Spotify Brand Logo",
+  },
+  {
+    id: 6,
+    name: "uber",
+    alt: "Uber Brand Logo",
+  },
+  {
+    id: 7,
+    name: "youtube",
+    alt: "YouTube Brand Logo",
+  },
+];
