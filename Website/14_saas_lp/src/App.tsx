f19ebd5ea6f7c8e3d234056658e4098afdba94a0
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-25 23:52:56 +0700

    chore(brands): setup folder structure and import libraries

--- Website/14_saas_lp/src/App.tsx
+++ Website/14_saas_lp/src/App.tsx
@@ -1,11 +1,13 @@
 import type { FC } from "react";
 import Layout from "./components/Layout";
 import { Hero } from "./components/sections/Hero";
+import { Brands } from "./components/sections/Brands";
 
 const App: FC = () => {
   return (
     <Layout title="EdgeAI">
       <Hero />
+      <Brands />
     </Layout>
   );
 };
