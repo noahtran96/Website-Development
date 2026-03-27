f40c0366f52ee6c536934184242e1fc992897672
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-27 14:02:22 +0700

    fix(info+service): fix icon display in light mode

--- Website/14_saas_lp/src/components/cards/Info.tsx
+++ Website/14_saas_lp/src/components/cards/Info.tsx
@@ -10,7 +10,7 @@ interface InfoProps {
 export const Info: FC<InfoProps> = ({ title, description, children }) => {
   return (
     <div className="p-5 sm:p-6 lg:p-8 rounded-3xl border border-box-border bg-box-bg shadow-lg shadow-box-shadow relative overflow-hidden">
-      <div className="rounded-xl bg-gray dark:bg_gray_950 p-3 text-heading-1 w-max relative">
+      <div className="rounded-xl bg:body p-3 text-heading-1 w-max relative">
         {children}
       </div>
       <h2 className="text-heading-2 w-max relative font-semibold md:text-xl">
--- Website/14_saas_lp/src/components/cards/Service.tsx
+++ Website/14_saas_lp/src/components/cards/Service.tsx
@@ -10,7 +10,7 @@ interface ServiceProps {
 export const Service: FC<ServiceProps> = ({ title, description, icon }) => {
   return (
     <div className="p-5 sm:p-6 lg:p-8 rounded-3xl border border-box-border bg-box-bg shadow-lg shadow-box-shadow relative overflow-hidden">
-      <div className="rounded-xl bg-gray-300 dark:bg-gray-950 p-3 text-heading-1 w-max relative">
+      <div className="rounded-xl bg-body p-3 text-heading-1 w-max relative">
         {icon}
       </div>
       <div className="mt-6 space-y-4 relative">
