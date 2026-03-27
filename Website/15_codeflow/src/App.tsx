f0f7f73e70d5b0c1d1a3a1f3872ca5c7fb36748f
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-27 18:38:36 +0700

    chore(global): setup project and import libraries

---
+++ Website/15_codeflow/src/App.tsx
@@ -0,0 +1,20 @@
+import type { FC } from "react";
+import { Navbar } from "./components/Navbar";
+import { Hero } from "./components/Hero";
+import { Features } from "./components/Features";
+import { Pricing } from "./components/Pricing";
+import { Testimonials } from "./components/Testimonials";
+import { Footer } from "./components/Footer";
+
+export const App: FC = () => {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Testimonials />
+      <Footer />
+    </div>
+  );
+};
---
+++ Website/15_codeflow/src/components/Navbar.tsx
@@ -0,0 +1,5 @@
+import type { FC } from "react";
+
+export const Navbar: FC = () => {
+  return <div>kdfjdkjsf</div>;
+};
