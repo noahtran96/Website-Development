98e2354a7705b3067e39ba86cdbaad440f2b57a5
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-03-27 10:59:13 +0700

    feat(about-us): setup structure with styling

--- Website/14_saas_lp/src/constants/navItems.ts
+++ Website/14_saas_lp/src/constants/navItems.ts
@@ -7,5 +7,5 @@ export const NAV_ITEMS: NavItemConfig[] = [
   { href: "#", text: "Home" },
   { href: "#services", text: "Services" },
   { href: "#about-us", text: "About Us" },
-  { href: "#features", text: "Features" },
+  { href: "#pricing", text: "Pricing" },
 ];
