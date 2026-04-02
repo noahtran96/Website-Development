5aef39f64187786c933e9b6809fb74aa5fa2d539
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-04-02 22:56:10 +0700

    chore(testimonialItems): setup data file

--- Website/15_codeflow/src/constants/index.ts
+++ Website/15_codeflow/src/constants/index.ts
@@ -2,3 +2,4 @@ export * from "./navItems";
 export * from "./codeExamples";
 export * from "./features";
 export * from "./pricingPlans";
+export * from "./testimonialItems";
---
+++ Website/15_codeflow/src/constants/testimonialItems.ts
@@ -0,0 +1,33 @@
+export interface TestimonialItem {
+  name: string;
+  role: string;
+  image: string;
+  content: string;
+}
+
+export const TESTIMONIAL_ITEMS: TestimonialItem[] = [
+  {
+    name: "Sarah Chen",
+    role: "Senior Developer",
+    image:
+      "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=200",
+    content:
+      "This AI-powered development platform has revolutionized how we write code. The intelligent suggestions save us hours every day.",
+  },
+  {
+    name: "Marcus Rodriguez",
+    role: "Tech Lead",
+    image:
+      "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=200",
+    content:
+      "The automated testing and debugging features are game-changers. We ship code faster and with more confidence than ever before.",
+  },
+  {
+    name: "Emily Watson",
+    role: "CTO",
+    image:
+      "https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=200",
+    content:
+      "Our development velocity increased by 300% since adopting this platform. It's like having a senior developer pair programming with every team member.",
+  },
+];
