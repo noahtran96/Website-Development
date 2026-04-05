e14e51d7579ae212f86aa9c93b934420d18a170c
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-04-05 23:22:50 +0700

    feat(home): setup effect hooks to process data

--- Website/16_crypto_tracker/src/pages/Home.tsx
+++ Website/16_crypto_tracker/src/pages/Home.tsx
@@ -1,3 +1,20 @@
+import { useEffect, useState } from "react";
+import { fetchCryptos } from "../api/coinGecko";
+
 export const Home = () => {
+  const [cryptoList, setCryptoList] = useState([]);
+
+  useEffect(() => {
+    const fetchCryptoData = async () => {
+      try {
+        const data = await fetchCryptos();
+        setCryptoList(data);
+      } catch (err) {
+        console.error(`Error fetching crypto: ${err}`);
+      }
+    };
+    fetchCryptoData();
+  }, []);
+
   return <div>This is the home page</div>;
 };
