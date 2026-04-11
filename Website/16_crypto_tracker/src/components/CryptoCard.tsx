177781cdb17824a3df8582c1301b139c2659b294
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-04-11 23:44:01 +0700

    feat(routing): setup routing to detail page

--- Website/16_crypto_tracker/src/components/CryptoCard.tsx
+++ Website/16_crypto_tracker/src/components/CryptoCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import type { Crypto } from "../api/coinGecko";
 import { formatPrice, formatMarketCap } from "../utils/formatter";
 
@@ -7,48 +8,50 @@ interface CryptoCardProps {
 
 export const CryptoCard = ({ crypto }: CryptoCardProps) => {
   return (
-    <div className="crypto-card">
-      {/* crypto header */}
-      <div className="crypto-header">
-        <div className="crypto-info">
-          <img src={crypto.image} alt={crypto.name} />
-          <div>
-            <h3>{crypto.name}</h3>
-            <p className="symbol">{crypto.symbol.toUpperCase()}</p>
-            <span className="rank">{crypto.market_cap_rank}</span>
+    <Link to={`/coin/${crypto.id}`} style={{ textDecoration: "none" }}>
+      <div className="crypto-card">
+        {/* crypto header */}
+        <div className="crypto-header">
+          <div className="crypto-info">
+            <img src={crypto.image} alt={crypto.name} />
+            <div>
+              <h3>{crypto.name}</h3>
+              <p className="symbol">{crypto.symbol.toUpperCase()}</p>
+              <span className="rank">{crypto.market_cap_rank}</span>
+            </div>
+          </div>
+        </div>
+
+        {/* crypto price */}
+        <div className="crypto-price">
+          <p className="price">{formatPrice(crypto.current_price)}</p>
+          <p
+            className={`change ${crypto.price_change_percentage_24h >= 0 ? "positive" : "negative"}`}
+          >
+            {crypto.price_change_percentage_24h >= 0 ? "↑" : "↓"}{" "}
+            {Math.abs(crypto.price_change_percentage_24h).toFixed(2)}%
+          </p>
+        </div>
+
+        {/* crypto stats */}
+        <div className="crypto-stats">
+          {/* stat 1 - market cap */}
+          <div className="stat">
+            <span className="stat-label">Market Cap</span>
+            <span className="stat-value">
+              {formatMarketCap(crypto.market_cap)}
+            </span>
+          </div>
+
+          {/* stat 2 - total volume */}
+          <div className="stat">
+            <span className="stat-label">Volume</span>
+            <span className="stat-value">
+              {formatMarketCap(crypto.total_volume)}
+            </span>
           </div>
         </div>
       </div>
-
-      {/* crypto price */}
-      <div className="crypto-price">
-        <p className="price">{formatPrice(crypto.current_price)}</p>
-        <p
-          className={`change ${crypto.price_change_percentage_24h >= 0 ? "positive" : "negative"}`}
-        >
-          {crypto.price_change_percentage_24h >= 0 ? "↑" : "↓"}{" "}
-          {Math.abs(crypto.price_change_percentage_24h).toFixed(2)}%
-        </p>
-      </div>
-
-      {/* crypto stats */}
-      <div className="crypto-stats">
-        {/* stat 1 - market cap */}
-        <div className="stat">
-          <span className="stat-label">Market Cap</span>
-          <span className="stat-value">
-            {formatMarketCap(crypto.market_cap)}
-          </span>
-        </div>
-
-        {/* stat 2 - total volume */}
-        <div className="stat">
-          <span className="stat-label">Volume</span>
-          <span className="stat-value">
-            {formatMarketCap(crypto.total_volume)}
-          </span>
-        </div>
-      </div>
-    </div>
+    </Link>
   );
 };
