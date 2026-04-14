73e2f32d704860e172f967cdf0cf0a49e710e5d0
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-04-14 12:48:46 +0700

    feat(app): setup basic component structure and layout

--- Website/17_HooBank/src/App.tsx
+++ Website/17_HooBank/src/App.tsx
@@ -1,3 +1,26 @@
-export default function App() {
-  return <div>HooBank</div>;
+import styles from "./style";
+
+function App() {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      {/* navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+
+      {/* hero */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+
+      {/* others */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats Business Billing CardDeal Testimonials Clients CTA Footer
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default App;
---
+++ Website/17_HooBank/src/constants/index.ts
@@ -0,0 +1,7 @@
+export * from "./clients";
+export * from "./features";
+export * from "./footerLinks";
+export * from "./navLinks";
+export * from "./socialMedia";
+export * from "./stats";
+export * from "./testimonials";
