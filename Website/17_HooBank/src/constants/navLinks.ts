a8ea4b1e577d8d757d29d1cec9a93739b3c4bb4a
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-04-14 12:21:03 +0700

    chore(navLinks): setup navLinks constants

---
+++ Website/17_HooBank/src/constants/navLinks.ts
@@ -0,0 +1,23 @@
+export interface navLink {
+  id: string;
+  title: string;
+}
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "features",
+    title: "Features",
+  },
+  {
+    id: "product",
+    title: "Product",
+  },
+  {
+    id: "clients",
+    title: "Clients",
+  },
+];
