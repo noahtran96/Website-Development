3f931f09bbcd40528f2b552c2af775b90d9624a6
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-04-14 12:32:14 +0700

    chore(clients): setup clients constants

---
+++ Website/17_HooBank/src/constants/clients.ts
@@ -0,0 +1,25 @@
+import { airbnb, binance, coinbase, dropbox } from "../assets";
+
+export interface Client {
+  id: string;
+  logo: string;
+}
+
+export const clients: Client[] = [
+  {
+    id: "client-1",
+    logo: airbnb,
+  },
+  {
+    id: "client-2",
+    logo: binance,
+  },
+  {
+    id: "client-3",
+    logo: coinbase,
+  },
+  {
+    id: "client-4",
+    logo: dropbox,
+  },
+];
