9e2387f84182c4a0854ca120b84a6b03eaac7700
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-04-14 12:31:17 +0700

    chore(socialMedia): setup social media constants

--- Website/17_HooBank/src/constants/index.js
+++ Website/17_HooBank/src/constants/index.js
@@ -9,29 +9,6 @@ import {
   dropbox,
 } from "../assets";
 
-export const socialMedia = [
-  {
-    id: "social-media-1",
-    icon: instagram,
-    link: "https://www.instagram.com/",
-  },
-  {
-    id: "social-media-2",
-    icon: facebook,
-    link: "https://www.facebook.com/",
-  },
-  {
-    id: "social-media-3",
-    icon: twitter,
-    link: "https://www.twitter.com/",
-  },
-  {
-    id: "social-media-4",
-    icon: linkedin,
-    link: "https://www.linkedin.com/",
-  },
-];
-
 export const clients = [
   {
     id: "client-1",
