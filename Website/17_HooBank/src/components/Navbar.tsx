51a2bb1ee4450f636ae1d2425eb8a679803e3a52
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-04-16 12:02:32 +0700

    feat(navbar): setup mobile responsive navbar with styling

--- Website/17_HooBank/src/components/Navbar.tsx
+++ Website/17_HooBank/src/components/Navbar.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { close, logo, menu } from "../assets";
 import { NAV_LINKS, type navLink } from "../constants";
 
 export const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="navbar flex w-full items-center justify-between py-6">
       <img src={logo} alt="HooBank logo" className="h-[32px] w-[124px]" />
@@ -15,6 +17,30 @@ export const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      {/* mobile responsive menu */}
+      <div className="flex flex-1 items-center justify-end sm:hidden">
+        <img
+          src={toggle ? close : menu}
+          alt="Mobile menu"
+          className="h-[28px] w-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${toggle ? "flex" : "hidden"} sidebar bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl p-6`}
+        >
+          <ul className="flex flex-1 list-none flex-col items-center justify-end gap-10">
+            {NAV_LINKS.map((nav: navLink) => (
+              <li
+                key={nav.id}
+                className="cursor-pointer text-[16px] font-normal text-white"
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
