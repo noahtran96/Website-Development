c9620868d3a0f7612c00c52a3a39533d76709fd8
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-04-17 23:47:18 +0700

    feat(hero/title): setup title with styling

--- Website/17_HooBank/src/components/Hero.tsx
+++ Website/17_HooBank/src/components/Hero.tsx
@@ -10,6 +10,7 @@ export const Hero = () => {
       <div
         className={`flex-1 ${styles.flexStart} flex-col px-6 sm:px-16 xl:px-0`}
       >
+        {/* discount badge */}
         <div className="bg-discount-gradient mb-2 flex flex-row items-center rounded-[10px] px-4 py-[6px]">
           <img
             src={discount}
@@ -20,6 +21,13 @@ export const Hero = () => {
             <span className="text-white">20%</span> Discount For{" "}
             <span className="text-white">1 Month</span> Account
           </p>
+        </div>
+
+        <div className="flex w-full flex-row items-center justify-between">
+          <h1 className="ss:text-[72px] ss:leading-[100px] flex-1 text-[52px] leading-[75px] font-semibold text-white">
+            The Next <br className="hidden sm:block" />{" "}
+            <span className="text-gradient">Generation</span> Payment Method.
+          </h1>
         </div>
       </div>
     </section>
