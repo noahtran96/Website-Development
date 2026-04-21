c681b16fc9a3f8b352fd5854371771569f1d5fc0
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-04-21 23:53:39 +0700

    feat(business/description): setup structure with styling

--- Website/17_HooBank/src/components/Business.tsx
+++ Website/17_HooBank/src/components/Business.tsx
@@ -1,3 +1,21 @@
+import { features } from "../constants";
+import styles, { layout } from "../style";
+import { Button } from "./Button";
+
 export const Business = () => {
-  return <div>Business</div>;
+  return (
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className="hidden sm:block" /> we'll handle
+          the money.
+        </h2>
+        <p className={`${styles.paragraph} mt-5 max-w-[470px]`}>
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+      </div>
+    </section>
+  );
 };
