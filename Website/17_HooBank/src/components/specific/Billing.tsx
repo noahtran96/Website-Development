b99b5dadf8bc6dc1c7cbce5a945e2665dd01e8f1
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-04-24 23:55:23 +0700

    feat(Billing/text): setup structure with styling

--- Website/17_HooBank/src/components/specific/Billing.tsx
+++ Website/17_HooBank/src/components/specific/Billing.tsx
@@ -19,7 +19,30 @@ export const Billing = () => {
       </div>
 
       {/* right side - text */}
-      <div></div>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Easily control your <br className="hidden sm:block" /> billing &
+          invoicing.
+        </h2>
+        <p className={`${styles.paragraph} mt-5 max-w-[470px]`}>
+          Manage all your payments and professional invoices in one place. Track
+          expenses, automate reminders, and stay on top of your finances with
+          just a few taps
+        </p>
+        {/* download icons */}
+        <div className="mt-6 flex flex-row flex-wrap sm:mt-10">
+          <img
+            src={apple}
+            alt="App Store icon"
+            className="mr-5 h-[42px] w-[128px] cursor-pointer object-contain"
+          />
+          <img
+            src={google}
+            alt="Google Play icon"
+            className="mr-5 h-[42px] w-[128px] cursor-pointer object-contain"
+          />
+        </div>
+      </div>
     </section>
   );
 };
