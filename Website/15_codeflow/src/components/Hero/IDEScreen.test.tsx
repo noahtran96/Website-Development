5d46cb39d7d3f8f29b255b19f11a1e48975a3469
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-05-06 23:56:25 +0700

    test(IDEScreen): import necessary libraries

---
+++ Website/15_codeflow/src/components/Hero/IDEScreen.test.tsx
@@ -0,0 +1,4 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import { IDEScreen } from "./IDEScreen";
+import { CODE_EXAMPLES, FLOATING_CARDS } from "../../constants";
