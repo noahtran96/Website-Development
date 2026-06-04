95f9ba1138a5bdb57a69f0b11dcf750058e32320
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-06-04 22:43:55 +0700

    feat: setup Clerk for user authentication

--- Website/18_Edemy/client/src/main.tsx
+++ Website/18_Edemy/client/src/main.tsx
@@ -4,13 +4,19 @@ import "./index.css";
 import App from "./App.tsx";
 import { AppContextProvider } from "./context/AppContext.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { ClerkProvider } from "@clerk/react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) throw new Error("Missing Publishable Key");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <AppContextProvider>
-        <App />
-      </AppContextProvider>
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </ClerkProvider>
     </BrowserRouter>
   </StrictMode>
 );
