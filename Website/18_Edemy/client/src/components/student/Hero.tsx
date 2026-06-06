7850901cee1fc9d05d6eab606fe43f29fc66fc48
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-06-07 01:51:29 +0700

    feat(Hero): setup Hero UI for desktop and mobile with custom font sizes

--- Website/18_Edemy/client/src/components/student/Hero.tsx
+++ Website/18_Edemy/client/src/components/student/Hero.tsx
@@ -1,5 +1,27 @@
-const Hero = () => {
-  return <div>Hero</div>;
-};
+import { assets } from "@/assets/assets";
 
-export default Hero;
+export const Hero = () => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center space-y-7 bg-gradient-to-b from-cyan-100/70 px-7 pt-20 text-center md:px-0 md:pt-36">
+      <h1 className="text-title-sm md:text-title-lg relative mx-auto max-w-3xl font-bold text-gray-800">
+        Empower your future with the courses designed to{" "}
+        <span className="text-blue-600">fit your choice.</span>
+        <img
+          src={assets.sketch}
+          alt="Sketch"
+          className="absolute right-0 -bottom-7 hidden md:block"
+        />
+      </h1>
+      <p className="mx-auto hidden max-w-2xl text-gray-500 md:block">
+        We bring together world-class instructors, interactive content, and a
+        supportive community to help you achieve your personal and professional
+        goals.
+      </p>
+      <p className="mx-auto max-w-sm text-gray-500 md:hidden">
+        We bring together world-class instructors, interactive content, and a
+        supportive community to help you achieve your personal and professional
+        goals.
+      </p>
+    </div>
+  );
+};
--- Website/18_Edemy/client/src/pages/student/Home.tsx
+++ Website/18_Edemy/client/src/pages/student/Home.tsx
@@ -1,3 +1,9 @@
+import { Hero } from "@/components/student/Hero";
+
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="flex flex-col items-center space-y-7 text-center">
+      <Hero />
+    </div>
+  );
 };
