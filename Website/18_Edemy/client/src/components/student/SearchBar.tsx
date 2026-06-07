921584d07a0e230e0d1ab2f88d53df486a777c5c
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-06-08 00:08:34 +0700

    feat(Hero/SearchBar): setup hooks for search bar functionality

--- Website/18_Edemy/client/src/components/student/SearchBar.tsx
+++ Website/18_Edemy/client/src/components/student/SearchBar.tsx
@@ -1,14 +1,27 @@
 import { assets } from "@/assets/assets";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const SearchBar = () => {
+export const SearchBar = ({ data }) => {
+  const navigate = useNavigate();
+  const [input, setInput] = useState(data ? data : "");
+  const onSearchHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    navigate("/course-list/" + input);
+  };
   return (
-    <form className="flex h-12 w-full max-w-xl items-center rounded border border-gray-500/20 bg-white md:h-14">
+    <form
+      onSubmit={onSearchHandler}
+      className="flex h-12 w-full max-w-xl items-center rounded border border-gray-500/20 bg-white md:h-14"
+    >
       <img
         src={assets.search_icon}
         alt="Search icon"
         className="w-10 px-3 md:w-auto"
       />
       <input
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
         type="text"
         placeholder="Search for courses"
         className="h-full w-full text-gray-500/80 outline-none"
