972d5bb6770a15cc35db2b3777540859a018f6ae
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-06-09 01:49:38 +0700

    feat(Home/Companies): setup companies UI

--- Website/18_Edemy/client/src/components/student/Companies.tsx
+++ Website/18_Edemy/client/src/components/student/Companies.tsx
@@ -1,5 +1,36 @@
-const Companies = () => {
-  return <div>Companies</div>;
-};
+import { assets } from "@/assets/assets";
 
-export default Companies;
+export const Companies = () => {
+  return (
+    <div className="pt-16">
+      <p className="text-base text-gray-500">Trusted by learners from</p>
+      <div className="mt-5 flex flex-wrap items-center justify-center gap-6 md:mt-10 md:gap-16">
+        <img
+          src={assets.microsoft_logo}
+          alt="Microsoft logo"
+          className="w-20 md:w-28"
+        />
+        <img
+          src={assets.walmart_logo}
+          alt="Walmart logo"
+          className="w-20 md:w-28"
+        />
+        <img
+          src={assets.accenture_logo}
+          alt="accenture logo"
+          className="w-20 md:w-28"
+        />
+        <img
+          src={assets.adobe_logo}
+          alt="Adobe logo"
+          className="w-20 md:w-28"
+        />
+        <img
+          src={assets.paypal_logo}
+          alt="PayPal logo"
+          className="w-20 md:w-28"
+        />
+      </div>
+    </div>
+  );
+};
--- Website/18_Edemy/client/src/pages/student/Home.tsx
+++ Website/18_Edemy/client/src/pages/student/Home.tsx
@@ -1,9 +1,11 @@
+import { Companies } from "@/components/student/Companies";
 import { Hero } from "@/components/student/Hero";
 
 export const Home = () => {
   return (
     <div className="flex flex-col items-center space-y-7 text-center">
       <Hero />
+      <Companies />
     </div>
   );
 };
