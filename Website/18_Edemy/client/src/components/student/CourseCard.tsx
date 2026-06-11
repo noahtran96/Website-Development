4ab7a7805e02acf233cfa607a0c2f18bd6c87ae9
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-06-11 23:54:51 +0700

    feat(CourseCard): setup course card UI

--- Website/18_Edemy/client/src/components/student/CourseCard.tsx
+++ Website/18_Edemy/client/src/components/student/CourseCard.tsx
@@ -1,3 +1,32 @@
-export const CourseCard = () => {
-  return <div>CourseCard</div>;
+import { assets } from "@/assets/assets";
+import { AppContext } from "@/context/AppContext";
+import { useContext } from "react";
+
+export const CourseCard = ({ course }) => {
+  const { currency } = useContext(AppContext);
+  return (
+    <div>
+      <img src={course.courseThumbnail} alt="Course thumbnail" />
+      <div>
+        <h3>{course.courseTitle}</h3>
+        <p>{course.educator.name}</p>
+        <div>
+          <p>4.5</p>
+          <div>
+            {[...Array(5)].map((_, index: number) => (
+              <img key={index} src={assets.star} alt="Rating star" />
+            ))}
+          </div>
+          <p>22</p>
+        </div>
+        <p>
+          {currency}
+          {(
+            course.coursePrice -
+            (course.discount * course.coursePrice) / 100
+          ).toFixed(2)}
+        </p>
+      </div>
+    </div>
+  );
 };
--- Website/18_Edemy/client/src/context/AppContext.tsx
+++ Website/18_Edemy/client/src/context/AppContext.tsx
@@ -3,7 +3,8 @@ import { createContext } from "react";
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
-  const value = {};
+  const currency = import.meta.env.VITE_CURRENCY;
+  const value = { currency };
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   );
