f15a9dd3f5adc62e19aa62a80c5769d0e1895b3b
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-06-16 18:15:17 +0700

    feat(Navbar): setup dropdown menu UI from user avatar

--- Website/19_dashboard/components/Navbar.tsx
+++ Website/19_dashboard/components/Navbar.tsx
@@ -1,6 +1,14 @@
-import { Moon } from "lucide-react"
+import { LogOut, Moon, Settings, User } from "lucide-react"
 import Link from "next/link"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu"
 
 export const Navbar = () => {
   return (
@@ -11,10 +19,32 @@ export const Navbar = () => {
       <div className="flex items-center gap-4">
         <Link href="/">Dashboard</Link>
         <Moon />
-        <Avatar>
-          <AvatarImage src="/Avatar.jpg" />
-          <AvatarFallback>NT</AvatarFallback>
-        </Avatar>
+
+        {/* Dropdown menu from avatar */}
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="/Avatar.jpg" />
+              <AvatarFallback>NT</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent sideOffset={10}>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <User className="mr-2 h-[1.2rem] w-[1.2rem]" />
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Settings className="mr-2 h-[1.2rem] w-[1.2rem]" />
+              Settings
+            </DropdownMenuItem>
+            <DropdownMenuItem variant="destructive">
+              <LogOut className="mr-2 h-[1.2rem] w-[1.2rem]" />
+              Logout
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </nav>
   )
