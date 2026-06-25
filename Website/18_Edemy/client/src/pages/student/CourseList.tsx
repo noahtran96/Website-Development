b7295fd65d00b3f10ae66ac37949ccc9934299ed
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-06-25 22:42:52 +0700

    feat(CourseList): setup clear filter UI and logic

--- Website/18_Edemy/client/src/pages/student/CourseList.tsx
+++ Website/18_Edemy/client/src/pages/student/CourseList.tsx
@@ -1,3 +1,4 @@
+import { assets } from "@/assets/assets";
 import { CourseCard } from "@/components/student/CourseCard";
 import { SearchBar } from "@/components/student/SearchBar";
 import { AppContext } from "@/context/AppContext";
@@ -38,6 +39,17 @@ export const CourseList = () => {
         </div>
 
         {/* Courses */}
+        {input && (
+          <div className="mt-8 -mb-8 inline-flex items-center gap-4 border px-4 py-2 text-gray-600">
+            <p>{input}</p>
+            <img
+              onClick={() => navigate("/course-list")}
+              src={assets.cross_icon}
+              alt="Cross icon"
+              className="cursor-pointer"
+            />
+          </div>
+        )}
         <div className="my-16 grid grid-cols-1 gap-3 px-2 sm:grid-cols-2 md:grid-cols-3 md:p-0 lg:grid-cols-4">
           {filteredCourse.map((course) => (
             <CourseCard key={course._id} course={course} />
