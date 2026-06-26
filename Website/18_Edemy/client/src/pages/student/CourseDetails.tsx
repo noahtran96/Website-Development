7453f9b28c92f076eb7b496d81fb9f9c5715f604
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-06-26 21:58:10 +0700

    feat(CourseDetails): setup reviews and ratings UI

--- Website/18_Edemy/client/src/pages/student/CourseDetails.tsx
+++ Website/18_Edemy/client/src/pages/student/CourseDetails.tsx
@@ -1,3 +1,4 @@
+import { assets } from "@/assets/assets";
 import { Loading } from "@/components/student";
 import { AppContext } from "@/context/AppContext";
 import { useContext, useMemo } from "react";
@@ -5,7 +6,7 @@ import { useParams } from "react-router-dom";
 
 export const CourseDetails = () => {
   const { id } = useParams();
-  const { allCourses } = useContext(AppContext);
+  const { allCourses, calculateRating } = useContext(AppContext);
   const courseData = useMemo(() => {
     if (!allCourses || allCourses.length === 0) return null;
     return allCourses.find((course) => course._id === id) || null;
@@ -27,6 +28,37 @@ export const CourseDetails = () => {
           }}
           className="pt-4 text-sm md:text-base"
         ></p>
+
+        {/* Reviews & ratings */}
+        <div className="flex items-center space-x-2 pt-3 pb-1 text-sm">
+          <p>{calculateRating(courseData)}</p>
+          <div className="flex">
+            {Array.from({ length: 5 }).map((_, index: number) => (
+              <img
+                key={index}
+                src={
+                  index < Math.floor(calculateRating(courseData))
+                    ? assets.star
+                    : assets.star_blank
+                }
+                alt="Rating star"
+                className="h-3.5 w-3.5"
+              />
+            ))}
+          </div>
+          <p className="text-blue-600">
+            ({courseData.courseRatings.length}{" "}
+            {courseData.courseRatings.length > 1 ? "ratings" : "rating"})
+          </p>
+          <p>
+            {courseData.enrolledStudents.length}{" "}
+            {courseData.enrolledStudents.length > 1 ? "students" : "student"}
+          </p>
+        </div>
+        <p className="text-sm">
+          Course by{" "}
+          <span className="text-blue-600 underline">{courseData.educator}</span>
+        </p>
       </div>
       {/* Right column */}
       <div className="relative z-20"></div>
