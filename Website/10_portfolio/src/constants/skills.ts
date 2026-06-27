5005a424585bcd11b0d7ff1e70c89a1aa46a97ef
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-06-27 10:58:34 +0700

    chore(skills): update skills

--- Website/10_portfolio/src/constants/skills.ts
+++ Website/10_portfolio/src/constants/skills.ts
@@ -4,10 +4,14 @@ export interface SkillType {
 }
 
 export const SKILLS: SkillType[] = [
-  { id: "1", name: "React" },
+  { id: "1", name: "JavaScript (ES6+)" },
   { id: "2", name: "TypeScript" },
-  { id: "3", name: "Tailwind CSS" },
-  { id: "4", name: "JavaScript (ES6+)" },
-  { id: "5", name: "HTML5" },
-  { id: "6", name: "CSS3" },
+  { id: "3", name: "React" },
+  { id: "4", name: "Next.js" },
+  { id: "5", name: "Tailwind CSS" },
+  { id: "6", name: "Node.js" },
+  { id: "7", name: "Express.js" },
+  { id: "8", name: "MongoDB" },
+  { id: "9", name: "Jest" },
+  { id: "10", name: "React Testing Library" },
 ];
