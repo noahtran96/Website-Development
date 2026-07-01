27e685db587f51ef8282ce850071e74313b5f54c
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-07-01 23:45:37 +0700

    feat(Loading): setup loading UI

--- Website/18_Edemy/client/src/components/student/Loading.tsx
+++ Website/18_Edemy/client/src/components/student/Loading.tsx
@@ -1,3 +1,7 @@
 export const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="aspect-square w-16 animate-spin rounded-full border-4 border-t-4 border-gray-300 border-t-blue-400 sm:w-20"></div>
+    </div>
+  );
 };
