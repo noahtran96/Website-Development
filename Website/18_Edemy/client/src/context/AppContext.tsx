8da829477de499215d1f8ff15a93f6c73c16f28a
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-07-02 22:24:17 +0700

    feat(MyEnrollments): setup UI

--- Website/18_Edemy/client/src/context/AppContext.tsx
+++ Website/18_Edemy/client/src/context/AppContext.tsx
@@ -10,6 +10,7 @@ export const AppContextProvider = (props) => {
   const navigate = useNavigate();
   const [allCourses, setAllCourses] = useState([]);
   const [isEducator, setIsEducator] = useState(true);
+  const [enrolledCourses, setEnrolledCourses] = useState([]);
 
   // Fetch all courses
   const fetchAllCourses = async () => {
--- Website/18_Edemy/client/src/pages/student/MyEnrollments.tsx
+++ Website/18_Edemy/client/src/pages/student/MyEnrollments.tsx
@@ -1,3 +1,19 @@
 export const MyEnrollments = () => {
-  return <div>MyEnrollments</div>;
+  return (
+    <>
+      <div className="px-8 pt-10 md:px-36">
+        <h1 className="text-2xl font-semibold">My Enrollments</h1>
+        <table className="mt-10 w-full table-fixed overflow-hidden border md:table-auto">
+          <thead className="border-b border-gray-500/20 text-left text-sm text-gray-900 max-sm:hidden">
+            <tr>
+              <th className="truncate px-4 py-3 font-semibold">Course</th>
+              <th className="truncate px-4 py-3 font-semibold">Duration</th>
+              <th className="truncate px-4 py-3 font-semibold">Completed</th>
+              <th className="truncate px-4 py-3 font-semibold">Status</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+    </>
+  );
 };
