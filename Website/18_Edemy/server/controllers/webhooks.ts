3cafc9137081e407693b23385e8bd0085de0a1d0
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-08-10 23:36:30 +0700

    feat(webhooks): setup logics

--- Website/18_Edemy/server/controllers/webhooks.ts
+++ Website/18_Edemy/server/controllers/webhooks.ts
@@ -26,9 +26,24 @@ export const clerkWebhooks = async (res, res) => {
         res.json({})
         break;
       }
-      default:
-        console.log("user is working")
-        console.log("user is created")
+      case "user.updated": {
+        const userData = {
+          email: data.email_address[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          imageUrl: data.image_url,
+        }
+        await User.findByIdAndUpdate(data.id, userData)
+        res.json({})
+        break;
+      }
+
+      case "user.deleted": {
+        await User.findByIdAndDelete(data.id)
+        res.json({})
+        break
+      }
+
+      default: break;
    
     }
   } catch (error) {}
