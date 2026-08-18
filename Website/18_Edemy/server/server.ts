dcd9ab3d04a73ac6a0241019b47a16e3b18c4da7
noahnoahnoahtran <tranthaingochuong@gmail.com>
2026-08-18 23:57:31 +0700

    test

--- Website/18_Edemy/server/server.ts
+++ Website/18_Edemy/server/server.ts
@@ -24,4 +24,5 @@ app.post("/clerk", express.json(), clerkWebhooks);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.error(error.message);
 });
